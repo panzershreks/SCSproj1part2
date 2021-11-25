c1e9e5272f0f048239fc4e3d494868084d7af1c5
Justin-3112 <s1905082@ed.ac.uk>
2021-11-25 22:05:37 +0000

    Updates to the modelling for beech

--- Project 2 code.R
+++ Project 2 code.R
@@ -1,4 +1,6 @@
 # Loading in and organising the data
+library(dplyr)
+
 data <- dat
 summary(data)
 data$tree_sp_eu <- factor(data$tree_sp_eu)
@@ -13,31 +15,26 @@ data_omit <- data_omit[-c(29)] #removes the SPEI for the 24-month period
 data_omit <- data_omit[-c(1)] #removes id
 data_omit <- na.omit(data_omit)
 summary(data_omit)
+data_omit_beech <- filter(data_omit, tree_sp_eu == "Rbu")
+data_omit_beech <- data_omit_beech[-c(4)]
+data_omit_spruce <- filter(data_omit, tree_sp_eu == "Gfi")
+data_omit_spruce <- data_omit_beech[-c(4)]
 
 
+# Analysis of data for Beech trees
+mod1 <- lm(nbv_ratio ~ ., data = data_omit_beech) #Model with all variables
+summary(mod1)
 
-
-
-# Analysis of data
-summary(lm1 <- lm(nbv_ratio ~ ., data = data_omit) )
-drop1(lm1, test = "F")
-
-
-lm_site <- lm(nbv_ratio ~ geol_no + soil_no + soil_ty_no + humus_no + water_no + nutri_no + slope_dir + skel_no + depth_no + alt_m + slope_deg, data = data_omit)
-summary(lm_site)
-
-mod1 <- lm(nbv_ratio ~ ., data = data_omit) #Model with all variables
 drop_mod1 <- drop1(mod1, test = "F")        #Backward selection using drop
 step_mod1 <- step(mod1)                     #Backward selection using step
 
+lm_site <- lm(nbv_ratio ~ geol_no + soil_no + soil_ty_no + humus_no + water_no + nutri_no + slope_dir + skel_no + depth_no + alt_m + slope_deg, data = data_omit_beech)
+summary(lm_site)
+
 par(mfrow = c(2,2))
 plot(step_mod1)    #plots the graphs to check model assumptions, Q-Q plots are odd at the end tail, residuals has a weird shape
-#there is one point that has a leverage of 1, could be an outlier
+#there are two points that have a leverage of 1, could be outliers (1590, 2422)
 
 
-mod2 <- lm(nbv_ratio ~ spei_12_oct, data = data_omit)
-summary(mod2)
 
-mod3 <- lm(nbv_ratio ~ spei_3_may + spei_3_aug, data = data_omit)
-summary(mod3)
 
