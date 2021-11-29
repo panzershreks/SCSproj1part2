a46f81cdec92c6cdf534c355f8f4d74b158b535f
panzershreks <weman@gmail.com>
2021-11-29 07:35:04 +0000

    Finished appendix

--- Proj1Part2Appendix.R
+++ Proj1Part2Appendix.R
@@ -7,20 +7,21 @@ library(car)
 library(olsrr)
 load(file = "foresthealth.rda")
 
-# ---------- Data ----------
+# ---------- Data ---------- #
 
 # Basic pre-processing
 data <- dat
 summary(data)
 data$tree_sp_eu <- factor(data$tree_sp_eu)
 data$id <- factor(data$id)
+levels(data$geol_no) <- c("MCB", "PGB", "NRC", "TrS", "TCS", "L", "TeS", "GFSLM", "LL")
 
 # Create data set for Beech
 data_beech <- filter(data, tree_sp_eu == "Rbu") # makes dataset for beech trees
 data_beech <- subset(data_beech, select = -c(H_spec, id, x_utm, y_utm, s_veg, prec_y, spei_12_oct, tree_sp_eu))
 data_beech <- na.omit(data_beech) # removing all the NA values
 
-# ---------- Model ---------- 
+# ---------- Model ---------- #
 
 # Model selection process
 full_mod <- lm(nbv_ratio ~ ., data = data_beech)
@@ -50,7 +51,7 @@ anova(rm_tmean_y, bic_add_spei24) # p-value of 0.0006639 < 1%
 # Final model
 model_final <- rm_alt_m
 
-# Check model assumptions
+# Check model assumptions (Figure 1)
 par(mfrow = c(2,2))
 plot(model_final)
 
@@ -84,6 +85,62 @@ defo_2016 <- data_beech %>%
   count()
 defo_2016 <- defo_2016 %>% mutate(freq = n/sum(defo_2016$n))
 defo_2016
+
+# Graphical Summaries (Figure 2)
+g1 <- (ggplot(data_beech, aes(year, nbv_ratio)) 
+       + geom_point() 
+       + stat_smooth(method = "lm") 
+       + theme(axis.title = element_text(size = 16))) 
+g3 <- (ggplot(data_beech, aes(prec_y_lag1, nbv_ratio)) 
+       + geom_point() 
+       + stat_smooth(method = "lm") 
+       + theme(axis.title = element_text(size = 16)))
+g6 <- (ggplot(data_beech, aes(geol_no, nbv_ratio)) 
+        + geom_boxplot() 
+        + scale_x_discrete(guide = guide_axis(n.dodge = 2),
+                           labels = c('MCB','PGB','NRC', 'TrS', 'TCS', 'L', 'TeS', 'GFSLM', 'LL'))
+        + theme(axis.title = element_text(size = 16)))
+g2 <- (ggplot(data_beech, aes(tree_age, nbv_ratio)) 
+       + geom_point()  
+       + stat_smooth(method = "lm") 
+       + theme(axis.title = element_text(size = 16))) 
+g4 <- (ggplot(data_beech, aes(tmin_may, nbv_ratio)) 
+       + geom_point()  
+       + stat_smooth(method = "lm") 
+       + theme(axis.title = element_text(size = 16))) 
+g5 <- (ggplot(data_beech, aes(et0_y_lag1, nbv_ratio)) 
+       + geom_point()  
+       + stat_smooth(method = "lm") 
+       + theme(axis.title = element_text(size = 16))) 
+g_plot <- ggarrange(g1, g2, g3, g4, g5, g6, ncol = 2, nrow = 3)
+g_plot
+
+
+# Geology distribution over all sites measured (Figure 3)
+geol_dist <- data_beech %>%
+  group_by(tpi500) %>%
+  slice(1L) %>%
+  group_by(geol_no) %>%
+  count()
+geol_dist <- geol_dist %>% mutate(freq = n/sum(geol_dist$n),
+                                  perc = freq*100)
+geol_dist
+
+geol_slice <- data_beech %>% group_by(tpi500) %>% slice(1L)
+ggplot(geol_slice, aes(x = geol_no)) +geom_bar()
+
+# ---------- Model Results ---------- #
+
+# Confidence Intervals
+Lower <- summary(model_final)$coefficients[,1] - 1.96*summary(model_final)$coefficients[,2]
+Upper <- summary(model_final)$coefficients[,1] + 1.96*summary(model_final)$coefficients[,2]
+Mean <- summary(model_final)$coefficients[,1]
+p_value <- summary(model_final)$coefficients[,4]
+CI <- cbind(Lower, Mean, Upper, p_value)[-1,]
+CI <- signif(CI, 3)
+ci_df <- data.frame(CI)
+#write.csv(ci_df, "./CI.csv", row.names = TRUE) # export df to .csv
+summary(model_final)
 
 
 
